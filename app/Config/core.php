ac937892da84af94c54724fd5114f992133d9518
Carlos Mapua <carlos.mapua@gmail.com>
2013-05-30 14:28:36 +0800

    remove core.php from repo

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'uSu*bur>AH!faN1w&zEXX7nFBi4Rl9/&LoWa*87u');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '151489934857833447560060989909');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
